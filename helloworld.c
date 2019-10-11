7ac25fc38f5a753fa4b53cd1e83e24ee2ed01450
smochan <56085742+smochan@users.noreply.github.com>
2019-10-11 13:28:01 +0530

    added how many times user wanna print hello world.

--- helloworld.c
+++ helloworld.c
@@ -2,6 +2,12 @@
 #include<conio.h>
 
 void main()
-clrscr();
-printf("hello world");
-getch();
+{
+  clrscr();
+  int a,i;
+  printf("enter how many times you wanna print hello world");
+  scanf("%d",&a);
+  for(i=1;i<=a;i++);
+      printf("hello world\n");
+  getch();
+}
